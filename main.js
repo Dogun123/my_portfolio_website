9ea77c266c29910754895a9348ac2ce9b7712bc3
Dogun123 <tpdnd7890@gmail.com>
2021-05-30 12:50:39 +0900

    JS Transparent home

--- main.js
+++ main.js
@@ -32,6 +32,14 @@ contactMe.addEventListener('click',()=>{
     scrollIntoView('#contact')
 });
 
+// Make home slowly fade to transparent as the window scrolls down
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll',()=>{
+    home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+
 // 스크롤 함수
 function scrollIntoView(selector) {
     const scrollTo = document.querySelector(selector);
