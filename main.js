8b89cde0e57029e10721cf55349665950ee9df70
Dogun123 <tpdnd7890@gmail.com>
2021-06-03 00:15:53 +0900

    JS: IntersectionObserver

--- main.js
+++ main.js
@@ -38,6 +38,7 @@ navbarMenu.addEventListener('click',(event)=>{
 
     navbarMenu.classList.remove('open');
     scrollIntoView(link)
+    selectNavItem(target);
 });
 
 // Navbar toggle button for small screen
@@ -93,8 +94,55 @@ workBtnContainer.addEventListener('click',(e)=>{
 
 });
 
+const sectionIds = [
+    '#home',
+    '#about',
+    '#skills',
+    '#work',
+    '#testimonials',
+    '#contact',
+];
 
+const sections = sectionIds.map(id=>document.querySelector(id));
+const navItems = sectionIds.map(id=>document.querySelector(`[data-link="${id}"]`))
+let selectedNavIndex=0;
+let selectedNavItem = navItems[0];
+function selectNavItem(selected) {
+    selectedNavItem.classList.remove('active');
+    selectedNavItem = selected;
+    selectedNavItem.classList.add('active');
+}
+const observerOptions = {
+    root: null,
+    rootMarging: '0px',
+    threshold: 0.3,
+}
 
+const observerCallback = (entries, observer) => {
+    entries.forEach(entry => {
+        if(!entry.isIntersecting && entry.intersectionRatio > 0){
+            const index = sectionIds.indexOf(`#${entry.target.id}`);
+            // 스크롤링이 아래로 되어서 페이지가 올라옴
+            if(entry.boundingClientRect.y<0) {
+                selectedNavIndex = index + 1;
+            } else {
+                selectedNavIndex = index - 1;
+            }    
+        }
+        
+    });
+};
+const observer = new IntersectionObserver(observerCallback,observerOptions);
+sections.forEach(section => observer.observe(section));
+
+window.addEventListener('wheel',()=>{
+    if(window.scrollY === 0) {
+        selectedNavIndex =0;
+    } else if (window.scrollY + window.innerHeight === document.body.clientHeight){
+        selectedNavIndex = navItems.length - 1;
+    }
+    selectNavItem(navItems[selectedNavIndex]);
+})
 // 스크롤 함수
 function scrollIntoView(selector) {
     const scrollTo = document.querySelector(selector);
